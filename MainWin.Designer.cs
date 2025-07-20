90f06b1ead140b9514d6a443624f117a8b7fea48
野 宋 <1571169588@qq.com>
2025-07-21 01:40:37 +0800

    添加项目文件。

---
+++ MainWin.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Bit_Locker
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWin));
+            this.normalLockButton = new System.Windows.Forms.Button();
+            this.forceLockButton = new System.Windows.Forms.Button();
+            this.driveChooser = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // normalLockButton
+            // 
+            this.normalLockButton.Location = new System.Drawing.Point(95, 224);
+            this.normalLockButton.Margin = new System.Windows.Forms.Padding(2);
+            this.normalLockButton.Name = "normalLockButton";
+            this.normalLockButton.Size = new System.Drawing.Size(143, 45);
+            this.normalLockButton.TabIndex = 0;
+            this.normalLockButton.Text = "普通锁定";
+            this.normalLockButton.UseVisualStyleBackColor = true;
+            this.normalLockButton.Click += new System.EventHandler(this.normalLockButton_Click);
+            // 
+            // forceLockButton
+            // 
+            this.forceLockButton.Location = new System.Drawing.Point(361, 224);
+            this.forceLockButton.Margin = new System.Windows.Forms.Padding(2);
+            this.forceLockButton.Name = "forceLockButton";
+            this.forceLockButton.Size = new System.Drawing.Size(143, 45);
+            this.forceLockButton.TabIndex = 1;
+            this.forceLockButton.Text = "强制锁定";
+            this.forceLockButton.UseVisualStyleBackColor = true;
+            this.forceLockButton.Click += new System.EventHandler(this.forceLockButton_Click);
+            // 
+            // driveChooser
+            // 
+            this.driveChooser.BackColor = System.Drawing.SystemColors.Info;
+            this.driveChooser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.driveChooser.FormattingEnabled = true;
+            this.driveChooser.ItemHeight = 12;
+            this.driveChooser.Location = new System.Drawing.Point(262, 113);
+            this.driveChooser.Margin = new System.Windows.Forms.Padding(2);
+            this.driveChooser.Name = "driveChooser";
+            this.driveChooser.Size = new System.Drawing.Size(76, 20);
+            this.driveChooser.TabIndex = 3;
+            this.driveChooser.SelectedIndexChanged += new System.EventHandler(this.driveChooser_SelectedIndexChanged);
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.driveChooser);
+            this.Controls.Add(this.forceLockButton);
+            this.Controls.Add(this.normalLockButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainWin";
+            this.Text = "Bitlocker锁定器";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button normalLockButton;
+        private System.Windows.Forms.Button forceLockButton;
+        private System.Windows.Forms.ComboBox driveChooser;
+    }
+}
+
---
+++ MainWin.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bit_Locker
+{
+    public partial class MainWin : Form
+    {
+        public string[] driveList = Environment.GetLogicalDrives();
+        List<string> bitlockerDrives = new List<string>();
+        public string targetDrive;
+        public MainWin()
+        {
+            InitializeComponent();
+            Console.Write("DriveList: "+string.Join(" ", driveList));
+            foreach (string drive in driveList)
+            {
+                
+                // 检查当前驱动器是否启用了 BitLocker
+                if (IsDriveBitlocked(drive))
+                {
+                    bitlockerDrives.Add(drive);
+                }
+            }
+            this.driveChooser.Items.AddRange(bitlockerDrives.ToArray());
+            
+            if(bitlockerDrives.Count > 0)
+            {
+                this.driveChooser.SelectedIndex = 0;
+                this.targetDrive = bitlockerDrives[0].Replace("\\", "");
+            }
+        }
+
+        private void driveChooser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.targetDrive = driveList[this.driveChooser.SelectedIndex].Replace("\\", "");
+        }
+
+        private void normalLockButton_Click(object sender, EventArgs e)
+        {
+            // 创建批处理文件内容
+            string batContent = $"%windir%\\Sysnative\\manage-bde.exe -lock {targetDrive} \r\n" +
+                                                     $"pause\n";
+
+            string tempPath = Path.GetTempPath();
+            string batFileName = "lock_drive.bat";
+            string batFilePath = Path.Combine(tempPath, batFileName);
+
+            File.WriteAllText(batFilePath, batContent, Encoding.ASCII);
+
+            try
+            {
+                RunProcess(batFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"启动进程时出错：{ex.Message}");
+            }
+        }
+
+        private void forceLockButton_Click(object sender, EventArgs e)
+        {
+            // 创建批处理文件内容
+            string batContent = $"%windir%\\Sysnative\\manage-bde.exe -lock {targetDrive} -ForceDismount\r\n" +
+                                                     $"pause\n";
+
+            string tempPath = Path.GetTempPath();
+            string batFileName = "lock_drive.bat";
+            string batFilePath = Path.Combine(tempPath, batFileName);
+
+            File.WriteAllText(batFilePath, batContent, Encoding.ASCII);
+
+            try
+            {
+                RunProcess(batFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"启动进程时出错：{ex.Message}");
+            }
+        }
+
+        private void RunProcess(string batFilePath)
+        {
+            // 检查文件路径是否为空
+            if (string.IsNullOrEmpty(batFilePath)) throw new ArgumentException("batFilePath cannot be null or empty.");
+
+            // 获取文件夹路径
+            string folderPath = System.IO.Path.GetDirectoryName(batFilePath);
+
+            // 检查文件夹是否存在
+            if (!System.IO.Directory.Exists(folderPath)) throw new DirectoryNotFoundException($"The folder '{folderPath}' does not exist.");
+
+            // 检查文件是否存在
+            string batFileName = System.IO.Path.Combine(folderPath, "lock_drive.bat");
+            if (!System.IO.File.Exists(batFileName)) throw new FileNotFoundException($"The file 'lock_drive.bat' does not exist in the folder '{folderPath}'.");
+
+            // 创建一个ProcessStartInfo对象
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = batFileName,  // 直接设置为批处理文件路径
+                UseShellExecute = true,  // 启用Shell执行
+                Verb = "runas",          // 请求管理员权限
+                WorkingDirectory = folderPath
+            };
+
+            try
+            {
+                // 启动进程
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                // 捕获异常并处理
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        // 检查指定驱动器是否启用了 BitLocker
+        private bool IsDriveBitlocked(string drive)
+        {
+            // 构建 WMI 查询
+            string query = $"SELECT * FROM Win32_EncryptableVolume WHERE DriveLetter = '{drive.Substring(0, 2)}'";
+
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions
+                {
+                    Impersonation = ImpersonationLevel.Impersonate,
+                    EnablePrivileges = true,
+                    Authentication = AuthenticationLevel.Packet
+                };
+
+                ManagementScope scope = new ManagementScope(@"root\cimv2\Security\MicrosoftVolumeEncryption", options);
+                scope.Connect();
+
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, new ObjectQuery(query)))
+                {
+                    foreach (ManagementObject volume in searcher.Get())
+                    {
+                        if ((UInt32?)volume["ProtectionStatus"] == 1) return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"查询驱动器 {drive} 时发生错误：{ex.Message}");
+            }
+
+            return false;
+        }
+    }
+}
